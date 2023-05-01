7681be9bc6ab41894e7b4038c4ac057060970588
Luke Finlayson <lukefin002@gmail.com>
2023-05-01 16:19:32 +1200

    Initial Commit

---
+++ KMPtable.java
@@ -0,0 +1,11 @@
+public class KMPtable {
+  public static void main(String[] args) {
+    // We only want one arg - the word to generate the table for
+    if (args.length != 1) {
+      System.err.println("Usage: java KMPtable [word]");
+      return;
+    }
+
+    
+  }
+}
